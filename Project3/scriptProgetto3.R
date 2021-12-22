af02519c5a9fe8cf11f49d5bc1356e34b575b02a
Simone Manti <manti998@gmail.com>
2021-12-22 11:48:38 +0100

    Added hw part and autovalidation for hw

--- Project3/scriptProgetto3.R
+++ Project3/scriptProgetto3.R
@@ -3,7 +3,7 @@ A = read.csv("RAILFRTINTERMODAL.csv", row.names = 1, stringsAsFactors = F)
 ts = ts(A, deltat=1/12, start = c(2000,1))
 
 #primo plot
-plot(ts)
+plot(ts, main = "Unità merci intermodali per trasporti ferroviari")
 
 #guardo l'autocorrelazione
 acf(ts, 30)
@@ -26,10 +26,10 @@ var(ts.dar)/var(window(ts,c(2000,7),c(2018,6)))
 var(ts.dmrl)/var(window(log(ts),c(2000,7),c(2018,6)))
 
 
-#vediamo se è presente stagionalità studiando la serie al netto del trend
-plot(diff(ts))
-acf(diff(ts))
-
+#proviamo ad utilizzare stagionalità variabile
+ts.stl = stl(ts[,1], 7)
+plot(ts.stl)
+plot(ts.stl$time.series[,3], main = "Serie dei residui")
 
 
 #proviamo ad utilizzare e confrontare HW additivo e moltiplicativo
@@ -44,6 +44,61 @@ print(ts.hwa$gamma)
 print(ts.hwm$alpha)
 print(ts.hwm$beta)
 print(ts.hwm$gamma)
+
+layout(t(1:2))
+# estrazione dei residui
+ts.hwa.r=resid(ts.hwa)
+ts.hwm.r=resid(ts.hwm)
+# proporzione di varianza non spiegata
+var(ts.hwa.r)/var(window(ts,1950))
+var(ts.hwm.r)/var(window(ts,1950))
+# rappresentazione grafica rispetto al tempo
+plot(ts.hwa.r, type = "p", pch = 20)
+plot(ts.hwm.r, type = "p", pch = 20)
+# rappresentazione grafica rispetto ai valori stimati
+plot(as.numeric(ts.hwa$fitted[,1]), as.numeric(ts.hwa.r),type="p",pch=20)
+plot(as.numeric(ts.hwm$fitted[,1]), as.numeric(ts.hwm.r),type="p",pch=20)
+# autocorrelazione
+acf(ts.hwa.r)
+acf(ts.hwm.r)
+# densità empiriche
+hist(ts.hwa.r, 20, freq = F)
+lines(density(ts.hwa.r),col="blue")
+lines(sort(ts.hwa.r), dnorm(sort(ts.hwa.r), mean(ts.hwa.r), sd(ts.hwa.r)), col = "red")
+hist(ts.hwm.r, 20, freq = F)
+lines(density(ts.hwm.r),col="blue")
+lines(sort(ts.hwm.r), dnorm(sort(ts.hwm.r), mean(ts.hwm.r), sd(ts.hwm.r)), col = "red")
+# grafico quantile-quantile
+qqnorm(ts.hwa.r, pch = 20)
+qqline(ts.hwa.r)
+qqnorm(ts.hwm.r, pch = 20)
+qqline(ts.hwm.r)
+# test
+shapiro.test(ts.hwa.r)
+shapiro.test(ts.hwm.r)
+layout(1)
+
+#autovalidation
+l=length(ts)
+res.hwa=rep(0,24)
+res.hwm=rep(0,24)
+j=1
+for(i in (l-24):(l-1)){
+  ap_cv=ts(ts[1:i],frequency=12,start=c(1949,1))
+  ts.hwa=HoltWinters(ts_cv,seasonal="additive")
+  ts.hwm=HoltWinters(ts_cv,seasonal="multiplicative")
+  ts.hwa.p=predict(ts.hwa,1)
+  ts.hwm.p=predict(ts.hwm,1)
+  res.hwa[j]=ts.hwa.p - ts[i+1]
+  res.hwm[j]=ts.hwm.p - ts[i+1]
+  j=j+1
+}
+sqrt(mean(res.hwa^2))
+sqrt(mean(res.hwm^2))
+plot(res.hwa,type="b",pch=20,col="blue")
+lines(res.hwm,type="b",pch=20,col="green3")
+
+
 
 
 
